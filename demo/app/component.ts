01e61dc28883331a4ddb3de937b0a125415719b0
Patrick Shields <pshields@gmail.com>
2018-09-02 18:06:14 -0500

    In demo app, use 'over' sidenav mode on handsets

--- demo/app/component.ts
+++ demo/app/component.ts
@@ -1,7 +1,12 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, ViewChild} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 
+import {MatSidenav} from '@angular/material';
 import {MatSnackBar} from '@angular/material';
+
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+
+import {filter} from 'rxjs/operators';
 
 import {DelegationMenuService} from 'lib/public_api';
 
@@ -12,8 +17,16 @@ import {DelegationMenuService} from 'lib/public_api';
   templateUrl: './component.html',
 })
 export class AppComponent {
+  // A reference to the app sidenav
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
+
+  // The mode of the Material Sidenav
+  // This is set based on the viewport size
+  // The initial value does not matter that much
+  sidenavMode = 'side';
 
   constructor(
+    readonly breakpointObserver: BreakpointObserver,
     readonly delegationMenuService: DelegationMenuService,
     readonly snackbar: MatSnackBar,
     readonly router: Router) {
@@ -24,5 +37,27 @@ export class AppComponent {
           this.snackbar.open('Delegated to the void');
         },
       });
+      // Listen for viewport size changes, and set the sidenav mode accordingly
+      breakpointObserver.observe([
+        Breakpoints.Handset,
+      ]).subscribe(result => this.setSidenavMode(result.matches));
+      // On handset screen sizes, close the sidenav after navigation events
+      this.router.events.pipe(
+        filter(e => e instanceof NavigationEnd)
+      ).subscribe(e => {
+        // Only close the sidenav if on a handset screen size, where
+        // screen real estate is at a premium
+        if (this.sidenavMode === 'over') {
+          this.sidenav.close();
+        }
+      });
+  }
+
+  setSidenavMode(isHandset: boolean) {
+    if (isHandset) {
+      this.sidenavMode = 'over';
+    } else {
+      this.sidenavMode = 'side';
     }
+  }
 }
