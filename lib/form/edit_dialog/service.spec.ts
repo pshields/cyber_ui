eb721c1b51c6cc9d304c9790d2fad238965acfaa
Patrick Shields <pshields@gmail.com>
2018-10-21 17:23:39 -0500

    Add another thing to test

--- lib/form/edit_dialog/service.spec.ts
+++ lib/form/edit_dialog/service.spec.ts
@@ -1,1 +1,2 @@
-// TODO it('works with a plain data object that does not have a save method')
+// TODO it('includes a default 'SAVE' action which calls the model's `save` method, when the model has such a method defined')
+// TODO it('works with a plain data object and a custom save option')
