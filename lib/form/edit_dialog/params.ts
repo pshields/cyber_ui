84e72684d43aba13318cdd34b231b4303768f5ff
Patrick Shields <pshields@gmail.com>
2018-10-21 16:36:26 -0500

    Allow for custom model type in CyberUiEditDialogParams

--- lib/form/edit_dialog/params.ts
+++ lib/form/edit_dialog/params.ts
@@ -3,15 +3,14 @@ import {Action} from '../../task/interfaces/action';
 import {FormField} from '../form_field';
 
 
-export interface CyberUiEditDialogParams {
+export interface CyberUiEditDialogParams<MODEL_T = {}> {
   // the (lower-case) label of the kind of thing being edited
   // this is used in the dialog title
   // e.g. if editing a task, contentTypeLabel should be 'task',
   // and the resulting dialog title will be 'Edit task'
   contentTypeLabel: string;
   // the data model object to bind to the form fields to
-  // TODO Find a better type for this, maybe?
-  model: {};
+  model: MODEL_T;
   // the field list to show in the dialog, if not provided on the model
   // (either the model instance or its class/constructor should
   // have a 'fields' property, or the 'fields' param should be
