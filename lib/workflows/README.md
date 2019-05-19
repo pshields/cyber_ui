568c12f9ebc41467027b1aeb8553ab5434db3e8d
Patrick Shields <pshields@gmail.com>
2019-05-19 17:51:09 -0500

    Think more about the add-thing workflow

--- lib/workflows/README.md
+++ lib/workflows/README.md
@@ -4,5 +4,11 @@
 
 Some preliminary thoughts:
 
-* URL path (e.g. `/workflows/work-on-tasks`, `/workflows/add-thing`)
 * An Angular component to initialize when the user is visiting that path
+* (Optional) URL path (e.g. `/workflows/work-on-tasks`, `/workflows/add-thing`)
+
+## How might a workflow be accessed?
+
+* Directly via URL
+* Through a link elsewhere in the app
+* Inside of a dialog (e.g. a Material dialog)
--- lib/workflows/add_thing/component.ts
+++ lib/workflows/add_thing/component.ts
@@ -3,7 +3,13 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'cyber-ui-add-thing-workflow',
-  templateUrl: 'component.html',
+  template: '',
 })
 export class CyberUiAddThingWorkflowComponent {
+  // TODO Inject a reference to the component to use to power this workflow
+  // This might be different depending on the application's theme
+  //
+  // Common display component choices might include:
+  // * cyber-ui-minimal-task-card
+  // * cyber-ui-form-fields
 }
