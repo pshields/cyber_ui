daa7bf51ab0c0a4ab344905ae1b40c192a325095
Patrick Shields <pshields@gmail.com>
2019-05-29 22:04:07 -0500

    minimal-card-list: show spinner until tasks load

--- lib/task/displays/minimal_card_list/module.ts
+++ lib/task/displays/minimal_card_list/module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import {MatProgressSpinnerModule} from '@angular/material';
+
 import {CyberUiMinimalTaskCardModule} from '../minimal_card/module';
 
 import {CyberUiMinimalTaskCardListComponent} from './component';
@@ -9,7 +11,8 @@ import {CyberUiMinimalTaskCardListComponent} from './component';
 @NgModule({
   imports: [
     CommonModule,
-    CyberUiMinimalTaskCardModule
+    CyberUiMinimalTaskCardModule,
+    MatProgressSpinnerModule,
   ],
   declarations: [
     CyberUiMinimalTaskCardListComponent,
