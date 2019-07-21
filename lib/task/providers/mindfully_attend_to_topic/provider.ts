2f990a8d57d9bbf74623b0c0ee4df9382ffe1068
Patrick Shields <pshields@gmail.com>
2019-07-21 08:34:41 -0500

    feat(task): start new mindfully_attend_to_topic task provider

---
+++ lib/task/providers/mindfully_attend_to_topic/provider.ts
@@ -0,0 +1,5 @@
+// Provides tasks around mindfully attending to topics
+// Example task statement:
+// Start a 50-minute hard focus session with the intention to mindfully attend to systemization, and log follow-ups as appropriate
+export class CyberUiMindfullyAttendToTopicTaskProvider {
+}
