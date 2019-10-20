7cd8a07b5b7410b1a7dd52bbe5eda29e257f74f8
Patrick Shields <pshields@gmail.com>
2019-10-20 10:06:54 -0500

    fix(edit_dialog): 'fields' input is now required

--- lib/form/edit_dialog/params.ts
+++ lib/form/edit_dialog/params.ts
@@ -1,10 +1,10 @@
+import {CyberUiSavableModel} from '../../backends/interfaces/savable_model';
 import {Action} from '../../task/interfaces/action';
 
 import {FormField} from '../form_field';
 
 
-// TODO MODEL_T should extend CyberUiSavableModel
-export interface CyberUiEditDialogParams<MODEL_T = {}> {
+export interface CyberUiEditDialogParams<MODEL_T extends CyberUiSavableModel = CyberUiSavableModel> {
   // the (lower-case) label of the kind of thing being edited
   // this is used in the dialog title
   // e.g. if editing a task, contentTypeLabel should be 'task',
@@ -12,11 +12,8 @@ export interface CyberUiEditDialogParams<MODEL_T = {}> {
   contentTypeLabel?: string;
   // the data model object to bind to the form fields to
   model: MODEL_T;
-  // the field list to show in the dialog, if not provided on the model
-  // (either the model instance or its class/constructor should
-  // have a 'fields' property, or the 'fields' param should be
-  // provided explicitly)
-  fields?: FormField<any, any, any>[];
+  // the fields to show in the dialog
+  fields: FormField<MODEL_T>[];
   // the actions to show in the mat-dialog-actions section
   actions?: Action[];
   // optional function to call when the user saves the form component and closes the dialog
