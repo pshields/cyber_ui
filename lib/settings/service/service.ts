531e74469b141600138a35a37dfd9c9d97bd287a
Patrick Shields <pshields@gmail.com>
2019-10-23 11:59:44 -0500

    fix(settings): clarify settings service interface

--- lib/settings/service/service.ts
+++ lib/settings/service/service.ts
@@ -7,14 +7,16 @@ import {map} from 'rxjs/operators';
 @Injectable({providedIn: 'root'})
 export class CyberUiSettingsService<SETTINGS_T = {}> {
   // A replay subject containing the current settings object
-  settings = new ReplaySubject<SETTINGS_T>(1);
+  private settings = new ReplaySubject<SETTINGS_T>(1);
 
+  // Consumers can call this method to update the global settings object
   onChange(settings: SETTINGS_T) {
     this.settings.next(settings);
   }
 
   // Returns a stream of settings optionally narrowed down by any provided property names
   // For example, listen('foo', 'bar') would provide a stream of settings.foo.bar values
+  // TODO Fix the any type here
   listen(...propertyNames: string[]): Observable<any> {
     return this.settings.pipe(map(settings => {
       for (let propertyName of propertyNames) {
