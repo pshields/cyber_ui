1257b25024014057b40f2281ba73b034b18f3836
Patrick Shields <pshields@gmail.com>
2019-11-19 04:55:36 -0600

    test(action): stub out test for useIcon input

---
+++ lib/action/ui/mat_buttons/component.spec.ts
@@ -0,0 +1,30 @@
+describe('CyberUiMatActionButtonsComponent', () => {
+
+  describe('useIcons input', () => {
+
+    describe('when true', () => {
+
+      it('displays provided icons', () => {
+        // TODO
+      });
+
+    });
+
+    describe('when false', () => {
+
+      it('does not display icons, even if they are provided', () => {
+        // TODO
+      });
+
+    });
+
+    describe('when undefined', () => {
+
+      it('defaults to true', () => {
+        // TODO
+      });
+    })
+
+  });
+
+});
